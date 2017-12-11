ff1f5f4cf42ff06e515f469e25afd2b1e5bf0768
jwnie <jwnie@foxmail.com>
2017-12-11 17:08:02 +0800

    first commit

---
+++ src/main/java/com/meow/proxy/ProxyserviceApplication.java
@@ -0,0 +1,12 @@
+package com.meow.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyserviceApplication.class, args);
+	}
+}
